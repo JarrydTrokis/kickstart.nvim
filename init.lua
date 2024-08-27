f5c57eab60a24f2851d2c7f1e06e7cce8fe12e74
Jarryd <jarryd.jay@gmail.com>
2024-08-27 16:58:04 +0100

    Adds netrw

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.g.have_nerd_font = true
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+vim.g.netrw_banner = 1
 
 -- Make line numbers default
 vim.opt.number = true
