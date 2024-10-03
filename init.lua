ca279281d419e51147d80b2339f8764f89e67da6
Jarryd <jarryd@attio.com>
2024-10-03 11:31:50 +0100

    update for formatting

--- init.lua
+++ init.lua
@@ -555,29 +555,47 @@ require('lazy').setup({
         desc = '[F]ormat buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    config = function()
+      local conform = require 'conform'
+      local jsFormatters = { 'prettierd', 'prettier', stop_after_first = true }
+
+      conform.setup {
+        formatters_by_ft = {
+          javascript = jsFormatters,
+          typescript = jsFormatters,
+          javascriptreact = jsFormatters,
+          typescriptreact = jsFormatters,
+          svelte = jsFormatters,
+          css = jsFormatters,
+          html = jsFormatters,
+          json = jsFormatters,
+          yaml = jsFormatters,
+          markdown = jsFormatters,
+          graphql = jsFormatters,
+          liquid = jsFormatters,
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+        },
+      }
+
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*',
+        group = vim.api.nvim_create_augroup('conform-autosave', { clear = true }),
+        callback = function(args)
+          conform.format {
+            bufnr = args.buf,
+          }
+        end,
+      })
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>mf', function()
+        conform.format {
+          lsp_fallback = true,
+          async = true,
+          timeout_ms = 1000,
         }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        typescript = { 'eslint', 'prettierd', 'prettier' },
-        javascript = { 'prettierd', 'prettier' },
-      },
-    },
+      end, { desc = '[F]ormat file or range (in visual mode)' })
+    end,
   },
 
   { -- Autocompletion
